92f0b0151fe29bb862bf8dcd364413c0d748213a
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-03 19:43:38 +0200

    [UPD]Add-render

--- js/scrip.js
+++ js/scrip.js
@@ -18,5 +18,23 @@ const oggetto = {
 //.push mette un elemento alla fine di un array per aggiungere le richieste api
 const request = []; //salvo le chiamate api
 for (let i = 0; i < 10; i++) {
+
+    //significa  chiedi a questo sito un email casuale
+    //e metti la risposta dentro request usando .push cosi la aggiunge alla fine
     request.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
 }
+
+//ora uso promise all per aspettare le richieste in modo che arrivano tuytte le email
+// non mostro le email fino a aquando non le ho tutte e 10
+Promise.all(request).then((response) => {
+    this.emails = responses.map(response => response.data.response);
+    this.WebGL2RenderingContext(); //dopo aver preso le mail aggiorno la pagina
+});
+
+//creo una funzione per stampare le mail usando render 
+
+render: function () {
+    const emailList = document.getElementById("email-list"); // Prendiamo la lista <ul>
+    emailList.innerHTML = this.emails.map(email => `<li>${email}</li>`).join("");
+  }
+  
